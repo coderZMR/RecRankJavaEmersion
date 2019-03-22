adb228c5bc71478eeedf6f9130983f153c944bfa
chichen <1021184207@qq.com>
2019-03-22 20:30:38 +0800

    update Main

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,18 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) throws IOException {
         Map<String, String> API2Index;
-        File api2index = new File("");
-        File dotFile = new File("");
-        File allPathFile = new File("");
-        File countPathFile = new File("");
+        File api2index = new File("/home/x/mydisk/RecRank/apiNew.txt");
+        File dotFile = new File("/home/x/mydisk/Gralan/wjwase/datafile2");
+        File allPathFile = new File("/home/x/mydisk/RecRank/allPathFile.txt");
+        File countPathFile = new File("/home/x/mydisk/RecRank/countPathFile.txt");
+        System.out.println("Read API2Index...");
         API2Index = ReadAPI2Index.getAPI2IndexMap(api2index);
+        System.out.println("Finish Read API2Index");
+        System.out.println("Write Path...");
         WritePathResult.ergodicDir(dotFile, allPathFile, API2Index);
+        System.out.println("Finish Write Path");
+        System.out.println("Read And Count PathResult...");
         ReadAndCountPathResult.readAndCount(allPathFile, countPathFile);
+        System.out.println("Finish Read And Count PathResult");
     }
 }
